51288c422ce4e6532551b437855b27ec50be4ea2
Nate Allen <neight.allen@gmail.com>
2015-02-18 12:11:58 -0600

    Updated toc scrollTo for header height

--- source/javascripts/app/toc.js
+++ source/javascripts/app/toc.js
@@ -15,7 +15,7 @@
       hideEffectSpeed: 180,
       ignoreSelector: '.toc-ignore',
       highlightOffset: 60,
-      scrollTo: -1,
+      scrollTo: parseInt($("nav").height()),
       scrollHistory: true,
       hashGenerator: function (text, element) {
         return element.prop('id');
